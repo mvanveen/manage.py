45c53a09a64d6eaf706e1d382e01c56100d6bee0
jean-philippe serafin <serafinjp@gmail.com>
2013-05-06 10:46:17 +0200

    Bumps 0.1b3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='manage.py',
-    version='0.1b2',
+    version='0.1b3',
     description='Human friendly CLI builder',
     long_description='',
     author='jean-philippe serafin',
